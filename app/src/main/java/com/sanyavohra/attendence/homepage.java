95a22c365a01fe8c2512e37941fac835c6e0c891
mradull <mradul.kapoor2004@gmail.com>
2023-10-24 13:26:45 +0530

    hihhiii

--- app/src/main/java/com/sanyavohra/attendence/homepage.java
+++ app/src/main/java/com/sanyavohra/attendence/homepage.java
@@ -8,19 +8,36 @@ import android.view.View;
 import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
-    Button button5;
+    Button button5,button4,button6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         button5= findViewById(R.id.button5);
+        button4= findViewById(R.id.button4);
+        button6= findViewById(R.id.button6);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(homepage.this,teacher_page.class);
                 startActivity(intent);
-                finish();
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homepage.this,admin.class);
+                startActivity(intent);
+
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homepage.this,student.class);
+                startActivity(intent);
+
             }
         });
     }
---
+++ app/src/main/java/com/sanyavohra/attendence/student.java
@@ -0,0 +1,14 @@
+package com.sanyavohra.attendence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+    }
+}
